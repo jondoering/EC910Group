38adb5a3343dc4e101c218f543c99c5d767dd544
Jonathan <jdoeri@essex.ac.uk>
2015-11-16 14:35:25 +0000

    First Draft of the Project

---
+++ EC910GroupProject/src/org/com/essex/ec910/artificialstockmarket/Starter.java
@@ -0,0 +1,11 @@
+package org.com.essex.ec910.artificialstockmarket;
+
+public class Starter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("CE910 Group Project - Implementing a artificial stock market \n\nProject Empty");
+		
+	}
+
+}
