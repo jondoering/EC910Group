b153104cb54be3f6daf901467216c8025caefc37
pouyan <pouyan@pouyan.com>
2015-12-10 00:51:07 +0000

    Abstract trader.java developed

--- EC910GroupProject/src/org/com/essex/ec910/artificialstockmarket/trader/AbstractTrader.java
+++ EC910GroupProject/src/org/com/essex/ec910/artificialstockmarket/trader/AbstractTrader.java
@@ -1,6 +1,47 @@
 package org.com.essex.ec910.artificialstockmarket.trader;
 
+import java.util.ArrayList;
+import org.com.essex.ec910.artificialstockmarket.market.ArtificialMarket;
+import org.com.essex.ec910.artificialstockmarket.market.Order;
+
+/**
+ * @author Pouyan
+ *
+ */
+
 public class AbstractTrader {
+	
+//	general parameters of trader
+	public String name;                         // trader's name eg: pouyan, Jonathan, randomTrader and ...
+	public ArtificialMarket artificialMarket;   // trader has access to artificial market
+	public Portfolio portfolio;                 // trader's portfolio 
+	public ArrayList<Order> openPositions;      // list of current open orders 
+	public ArrayList<Order> closedPositions;    // list of closed orders 
+
+//  metrics for measuring the performance of trading strategy
+	public int profit_loss;                     // P&L of trader
+	public int ROI;                             // return of investment 
+	public int numTrades;                       // number of total trades
+	public int numWinTrades;                    // number of winning trades
+	public int profitFactor;                    // = numWinTrades / numTrades
+	
+	/**
+	 * Constructor
+	 * @param name 
+	 * @param artificialMarket  
+	 * @param portfolio  
+	 */
+    public AbstractTrader(String name, ArtificialMarket artificialMarket, Portfolio portfolio) {
+		this.name = name;
+		this.artificialMarket = artificialMarket;
+		this.portfolio = portfolio;
+	}
+	
+//	run trading strategy
+	public void runStrategy(){
+		// ???
+	}
+
 
 	
 	
--- EC910GroupProject/src/org/com/essex/ec910/artificialstockmarket/trader/Portfolio.java
+++ EC910GroupProject/src/org/com/essex/ec910/artificialstockmarket/trader/Portfolio.java
@@ -9,7 +9,7 @@ public class Portfolio {
 
 
 	int shares;   // number of shares in trader's portfolio 	
-	int money;    // trader's capital (Integer)
+	int money;    // trader's capital
 		
 	/**
 	 * Constructor
@@ -24,12 +24,15 @@ public class Portfolio {
 	public int getShares() {
 		return shares;
 	}
+	
 	public void setShares(int shares) {
 		this.shares = shares;
 	}
+	
 	public int getMoney() {
 		return money;
 	}
+	
 	public void setMoney(int money) {
 		this.money = money;
 	}
