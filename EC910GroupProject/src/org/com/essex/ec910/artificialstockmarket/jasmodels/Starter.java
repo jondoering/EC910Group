52a8734a543e2626dde72f554447af3966932a5b
pouyan <pouyan@pouyan.com>
2015-12-12 02:59:53 +0000

    standard model.java created - please work on this model not Starter.java

--- EC910GroupProject/src/org/com/essex/ec910/artificialstockmarket/jasmodels/Starter.java
+++ EC910GroupProject/src/org/com/essex/ec910/artificialstockmarket/jasmodels/Starter.java
@@ -39,20 +39,21 @@ public int numberOfTrader;
 public void scheduleEvents() {
 
 }
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		SimEngine eng = new SimEngine();
-		JAS jas = new JAS(eng);
-		jas.setVisible(true);
-		
-		Starter m = new Starter();
-		eng.addModel(m);
-		m.setParameters();
-	
-		System.out.println("CE910 Group Project - Implementing a artificial stock market \n\nProject Empty");
-		
-		
-	}
+
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		
+//		SimEngine eng = new SimEngine();
+//		JAS jas = new JAS(eng);
+//		jas.setVisible(true);
+//		
+//		Starter m = new Starter();
+//		eng.addModel(m);
+//		m.setParameters();
+//	
+//		System.out.println("CE910 Group Project - Implementing a artificial stock market \n\nProject Empty");
+//		
+//		
+//	}
 
 }
--- EC910GroupProject/src/org/com/essex/ec910/artificialstockmarket/trader/RandomTrader.java
+++ EC910GroupProject/src/org/com/essex/ec910/artificialstockmarket/trader/RandomTrader.java
@@ -3,22 +3,54 @@ package org.com.essex.ec910.artificialstockmarket.trader;
 import org.com.essex.ec910.artificialstockmarket.market.ArtificialMarket;
 import org.com.essex.ec910.artificialstockmarket.market.Order;
 
+import jas.engine.Sim;
+
+
+// Random trader sends random order buy/sell to market and exits randomly 
+// Random trader is a source of liquidity for market
+
 public class RandomTrader extends AbstractTrader {
 
+
+	/**
+	 * Constructor
+	 * @param name 
+	 * @param artificialMarket  
+	 * @param portfolio  
+	 * @param max_buy
+	 * @param max_sell
+	 * 
+	 */
 	public RandomTrader(String name, ArtificialMarket artificialMarket, Portfolio portfolio, int max_buy,
 			int max_sell) {
+
 		super(name, artificialMarket, portfolio, max_buy, max_sell);
-		// TODO Auto-generated constructor stub
+
 	}
-	
+
 	@Override
 	public Order runStrategy(){
-        
-		Order order; 
-		
-		// your trading algorithm ...
-		
-		order = new Order(0, 0, 0, 0, this);// default order which will not be sent to the market (because volume = 0)
+
+		Order order;   //random market order to be sent to market 
+		int volume;    //random volume
+		int buyOrSell; //0= buy/ 1= sell  
+
+		if( Sim.getRnd().getIntFromTo(0, 1) == 0 ){ // 0 = open a position (buy or sell)
+
+			buyOrSell = Sim.getRnd().getIntFromTo(0, 1);//0=buy, 1=sell
+
+			if(buyOrSell == 0){// --> buy
+				volume = Sim.getRnd().getIntFromTo(1, this.max_buy);  // volume of trading is limited to 10 ?????????
+				order = new Order(buyOrSell, 2, volume, 0, this);
+			}
+			else{
+				volume = Sim.getRnd().getIntFromTo(1, this.max_sell);  // volume of trading is limited to 10 ?????????
+				order = new Order(buyOrSell, 2, volume, 0, this);// default order which will not be sent to the market (because volume = 0)
+			}  
+		}
+		else{
+			order = new Order(0, 0, 0, 0, this);// do nothing --> default order which will not be sent to the market (because volume = 0) 
+		}
 		return order;
 	}
 
