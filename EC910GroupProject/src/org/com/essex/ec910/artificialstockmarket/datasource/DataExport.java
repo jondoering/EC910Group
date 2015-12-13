802a63a8e24ed54bb6bf70fd5690f05fe98fb7d1
MAO WEIGUANG <maoweig@163.com>
2015-12-13 10:57:29 +0000

    use this class name instead of database connector

---
+++ EC910GroupProject/src/org/com/essex/ec910/artificialstockmarket/datasource/DataExport.java
@@ -0,0 +1,8 @@
+package org.com.essex.ec910.artificialstockmarket.datasource;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DataExport {
+
+}
