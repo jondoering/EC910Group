57a588d3e37f7a61093c3841baf969bd68cfb220
MAO WEIGUANG <maoweig@163.com>
2015-12-13 11:56:01 +0000

    update

--- EC910GroupProject/src/org/com/essex/ec910/artificialstockmarket/datasource/DataExport.java
+++ EC910GroupProject/src/org/com/essex/ec910/artificialstockmarket/datasource/DataExport.java
@@ -1,8 +1,35 @@
 package org.com.essex.ec910.artificialstockmarket.datasource;
 import java.sql.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class DataExport {
+	public static void main(String args[]) {
+	    try {
+	      Class.forName("com.mysql.jdbc.Driver");     
+	      //Class.forName("org.gjt.mm.mysql.Driver");
+	     System.out.println("Success loading Mysql Driver!");
+	    }
+	    catch (Exception e) {
+	      System.out.print("Error loading Mysql Driver!");
+	      e.printStackTrace();
+	    }
+//	    try {
+//	      Connection connect = DriverManager.getConnection(
+//	          "jdbc:mysql://localhost:3306/EC910Data","root","EC910");
+//	           
+//
+//	      System.out.println("Success connect Mysql server!");
+//	      Statement stmt = connect.createStatement();
+//	      ResultSet rs = stmt.executeQuery("select * from user");
+//	                                                              //user 为你表的名称
+//	while (rs.next()) {
+//	        System.out.println(rs.getString("name"));
+//	      }
+//	    }
+//	    catch (Exception e) {
+//	      System.out.print("get data error!");
+//	      e.printStackTrace();
+//	    }
+	  }
+	}
 
-}
+
