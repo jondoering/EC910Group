837d0263a2611f38ec40ad4a8df8854a19f38d99
MAO WEIGUANG <maoweig@163.com>
2016-01-17 13:28:09 +0000

    add comment

--- EC910GroupProject/src/org/com/essex/ec910/artificialstockmarket/strategy/WilliamTradingStrategy.java
+++ EC910GroupProject/src/org/com/essex/ec910/artificialstockmarket/strategy/WilliamTradingStrategy.java
@@ -17,71 +17,72 @@ import com.tictactec.ta.lib.MInteger;
 import com.tictactec.ta.lib.RetCode;
 
 /**
- * simple moving average strategy compare  
+ * counter Trend Strategy 
  * without explicit money management 
  * @author WEIGUANG MAO
- *
+ * buy share when price achieve the average price
+ * sell shares when price is higher than the average price 2%
  */
 public class WilliamTradingStrategy extends AbstractTrader {
 
-	
+
 	private double sellPercent;
-	
-	
+
+
 	/**
 	 * Ta Lib Core Object
 	 */
 	private Core talib;
 	private int period;
-	
-	
+
+
 	public WilliamTradingStrategy(final String name,final ArtificialMarket artificialMarket, final Portfolio portfolio, final int max_buy,
 			final int max_sell,final double buyPercentd, final int period) {
 		super(name, artificialMarket, portfolio, max_buy, max_sell);		
-		
+
 		this.sellPercent = buyPercentd;
 		this.period = period;
-		
+
 		talib = new Core();
 	}
 
-	
+
 	/* 
-	 * Simple SMA Strategy
+	 * 
 	 * (non-Javadoc)
 	 * @see org.com.essex.ec910.artificialstockmarket.trader.AbstractTrader#runStrategy()
 	 */
 	@Override
 	public Order runStrategy(){
-			 
+
 		Order order = new Order(Order.BUY, Order.MARKET, 0, 0, this);// default order which will not be sent to the market (because volume = 0)
 
 		// get Prices	
-		
+
 		double[] prices = artificialMarket.getLastNPriceAsDoubles(period);
-				
-		
+
+
 		if(prices != null)
 		{
-			
+
 			double spotprice = artificialMarket.getSpotPriceAsDouble();
-			
+
 			if(prices.length >= period)
 			{
 				double[] outLong = new double[prices.length];
-				
+
 				MInteger begin = new MInteger();				
 				MInteger length = new MInteger();
-				
+
 				//Calculate SMAs
 				RetCode retCode = talib.sma(0, prices.length-1, prices, period, begin, length, outLong);				
-				
-				
+
+
 				if(retCode == RetCode.Success)
 				{
 					if(spotprice <= outLong[0]  && this.portfolio.getShares() == 0 ) 
 					{
-						//Buy as much as possible if price is close to moving averagh
+						//Buy as much as possible if price is close to moving average
 						long vol = (long) Math.floor(this.getInvestableMoney()/spotprice);
 
 						if(vol > max_buy)
@@ -89,26 +90,20 @@ public class WilliamTradingStrategy extends AbstractTrader {
 
 						order = new Order(Order.BUY, Order.MARKET, vol, (int) spotprice, this);
 					}
-					
+
 					if(spotprice >= outLong[0]*(1+sellPercent) && this.portfolio.getShares() > 0)
 					{
 						//Sell all shares if current price is sellPercent above the average 
 						order = new Order(Order.SELL, Order.MARKET, portfolio.getShares(), (int) spotprice, this);
-						
+
 					}
-					
+
 				}
-				
+
 			}
 
 		}
 		return order;
 	}
-	
-	
 
-
-
-	
-	
 }
